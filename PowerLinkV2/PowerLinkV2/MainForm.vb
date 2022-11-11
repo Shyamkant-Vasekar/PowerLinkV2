0c04c8ddd9800df5f8aedf3293507d3ecdb81099
GecaLearner <vasekar@geca.ac.in>
2022-11-11 19:21:37 +0530

    Change con string
    Connection string is changed as service provider changed setting.

--- PowerLinkV2/PowerLinkV2/MainForm.vb
+++ PowerLinkV2/PowerLinkV2/MainForm.vb
@@ -81,7 +81,7 @@ Public Class MainForm
     Public PwrDataSetR As DataSet
     Public PwrDataTableR As DataTable
     Public PwrDataAdapterR As SqlDataAdapter
-    Public ConStrR As String = "Data Source=148.72.232.168;Integrated Security=False;User ID=spvasekar; Password=Mydatabasepassword#1"
+    Public ConStrR As String = "Data Source=YOUR_SERVER_USERNAME_PSW;Integrated Security=False;User ID=spvasekar; Password=Mydatabasepassword#1"
     Public conR As SqlConnection
 
 
--- PowerLinkV2/PowerLinkV2/My Project/AssemblyInfo.vb
+++ PowerLinkV2/PowerLinkV2/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("90d4f334-3ac6-4f4a-8d64-a3d12e99a2d6")> 
+<Assembly: Guid("90d4f334-3ac6-4f4a-8d64-a3d12e99a2d6")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.1.0.5")> 
-<Assembly: AssemblyFileVersion("2.1.0.5")> 
+<Assembly: AssemblyVersion("2.1.0.6")>
+<Assembly: AssemblyFileVersion("2.1.0.6")>
